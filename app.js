7b9034c107f31ab9c1ceceeffd78e21d2440f611
Ehsan Javdan <ehsanjavdan77@gmail.com>
2026-05-15 20:11:57 +0400

    added some security packages which are helmet, cors and rate limiter and implemented them

--- app.js
+++ app.js
@@ -5,6 +5,21 @@ const menuItemRouter = require("./routes/menu_item_routes");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const rateLimiter = require("express-rate-limit");
+
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests
+    message: "Too many requests, please try again later.",
+  }),
+);
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/menu-items", menuItemRouter);
